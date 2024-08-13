62059477f412bb12f5cc2d0f079914ea310546f0
Leonardo Santos <leonardojosantos@gmail.com>
2024-08-13 14:19:55 -0300

    tarefas - exercício 3: cubo de um número (js)

--- tarefas/01_tarefas_sem_data/02_js/03_cubo_de_um_numero/assets/js/index.js
+++ tarefas/01_tarefas_sem_data/02_js/03_cubo_de_um_numero/assets/js/index.js
@@ -2,6 +2,10 @@
  * Escreva uma função que retorne o cubo de um número fornecido pelo usuário. 
  **/
 
+function calculaCubo(numero) {
+    return Math.pow(numero, 3)
+}
+
 let numeroRecebido = window.prompt('Informe um número para obter seu cubo: ');
 
 if (numeroRecebido === '') {
@@ -12,7 +16,7 @@ else if (numeroRecebido === null) {
 }
 else {
     numeroRecebido = Number(Number(numeroRecebido.replace(',', '.')).toFixed(2));
-    const cuboNumeroRecebido = Number(Math.pow(numeroRecebido, 3).toFixed(2));
+    const cuboNumeroRecebido = Number(calculaCubo(numeroRecebido).toFixed(2));
     console.log('Número recebido..........:', numeroRecebido);
     console.log('Cubo do número recebido..:', cuboNumeroRecebido);
 }
