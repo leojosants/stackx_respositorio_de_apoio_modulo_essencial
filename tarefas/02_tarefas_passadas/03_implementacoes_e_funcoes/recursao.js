08c0dad0ae633a0d3fd14ad869ed3c66cc990f32
Leonardo Santos <90782310+leojosants@users.noreply.github.com>
2024-08-28 01:00:51 -0300

    Update recursao.js

--- tarefas/02_tarefas_passadas/03_implementacoes_e_funcoes/recursao.js
+++ tarefas/02_tarefas_passadas/03_implementacoes_e_funcoes/recursao.js
@@ -24,43 +24,12 @@ function ehPar(numero) {
 }
 // ehPar(numero);
 
-// frame base
 function fibonacci(n) {
   if (n <= 1) {
     return 1;
   }
   return fibonacci(n - 1) + fibonacci(n - 2);
-  //               9      +           8
 };
-
-// 0 1 2 3 4 5 6  7  8  9  10
-// 1 1 2 3 5 8 13 21 34 55 89
-
-
 // console.log(fibonacci(10));
 
 module.exports = { somaNumeros, ehPar, fibonacci };
-
-
-
-
-
-
-var fibonacci = function (n) {
-  var a = 0, b = 1;
-  for (var posicao = 2; posicao <= n; posicao++) {
-
-    var f = a;
-    console.log('anterior:', f);
-
-    a = b;
-    console.log('atual:', a);
-
-    b += f;
-    console.log('f+a:', f + a);
-    console.log('posicao:', posicao);
-  }
-  return b;
-};
-
-console.log(fibonacci(10));
